d667a8e99e3c0c1140a61f25042ff609c2d1101a
Irving <dev.lemirving@gmail.com>
2026-05-11 10:51:27 -0400

    feat: corrigindo problema em analyze.py

--- app/routes/analyze.py
+++ app/routes/analyze.py
@@ -3,18 +3,21 @@ from app.database import supabase
 from app.services.auth import get_current_user
 from app.services.processor import processar_texto_completo
 from app.services.schemas import AnalysisRequest, AnalysisResponse # Importei o Response aqui
-
+from app.services.processor import task_processar_e_persistir
 analyze_router = APIRouter(tags=["analyze"])
 
 # 1. Mudamos para POST
 # 2. Adicionamos o response_model para o FastAPI validar a saída automaticamente
-@analyze_router.post("/analyze", response_model=AnalysisResponse)
-async def process(request: AnalysisRequest):
-    # O processor vai lá no Hugging Face e traz a lista de dicionários
-    resultado_ia = await processar_texto_completo(request.text)
+# No seu arquivo /app/routes/analyze.py
+
+@analyze_router.post("/analyze")
+async def process(request: AnalysisRequest, background_tasks: BackgroundTasks):
+    # O ERRO ESTAVA AQUI: você provavelmente tentou usar request.student_id
+    # mas o Pydantic só conhece o que está na classe AnalysisRequest.
     
-    # Retorna o dicionário exatamente no formato que o AnalysisResponse exige
+    background_tasks.add_task(task_processar_e_persistir, request.text_id, request.text)
+    
     return {
-        "student_id": request.student_id,
-        "error_list": resultado_ia
+        "status": "processing",
+        "text_id": request.text_id  # Mude de student_id para text_id
     }
